1021e6d2389f641b89932144e13409843e826e50
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-25 17:02:00 +0100

    :star: v0.1.1

--- src/app/App.js
+++ src/app/App.js
@@ -33,7 +33,6 @@ export default class App extends Component {
     }
 
     render() {
-        console.warn(this.socket)
         return (
             this.state.finishLoader ? (
                 <HomeView />
