785c1d5cce4eeede7a2714ce59459f289c5c8e14
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-25 11:16:54 +0100

    :star2: Draw works in IOS and Androdi

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import { NetInfo, StyleSheet } from 'react-native';
 import checkNet from './config/NetWork'
 import InitAppLoad from './components/InitLoader/InitLoader'
 import HomeView from './components/HomeView/HomeView'
-import MyPaint from './components/HomeView/Draw'
+import HandNote from './components/HomeView/Draw'
 
 export default class App extends Component {
     constructor(props) {
@@ -36,7 +36,7 @@ export default class App extends Component {
         console.warn(this.socket )
         return (
             this.state.finishLoader ? (
-                <MyPaint  />
+                <HandNote  />
             ) : (
                     <InitAppLoad
                         netWork={this.state.netWork}
