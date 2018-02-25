af0f973aa40c60870f861e1bd93562740d4d7f7b
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-25 13:02:02 +0100

    delete-socket

--- src/app/App.js
+++ src/app/App.js
@@ -34,7 +34,6 @@ export default class App extends Component {
     }
 
     render() {
-        console.warn(this.socket)
         return (
             this.state.finishLoader ? (
                 // <HomeView />
